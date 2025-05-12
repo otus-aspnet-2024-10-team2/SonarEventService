1dad9e6a604c1718a0d83fa39137ee266dc17d16
myinc0876 <73604551+myinc0876@users.noreply.github.com>
2025-05-13 00:44:05 +0300

    Подбил структуру  БД

--- Infrastructure/SQLScripts/SqlLiteWorkScripts.sql
+++ Infrastructure/SQLScripts/SqlLiteWorkScripts.sql
@@ -1,34 +1,75 @@
-﻿-- Script Date: 22.01.2025 16:42  - ErikEJ.SqlCeScripting version 3.5.2.95
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 
 SELECT [Id]
       ,[Subject] 
       ,[CourseId]
       ,[Deleted]
-  FROM [SonarTasks];
+  FROM [SonarTasks]
   
  --
 
  select * from SonarProcess;
  
- 
-
-
-
-
-
-
-
-
-
-
-
-
 
  
+ 
+ SELECT 
+    st.Id AS TaskId,
+    st.Title AS TaskTitle,
+    st.Description AS TaskDescription,
+    st.Status AS TaskStatus,
+    st.CreatedAt AS TaskCreatedAt,
+    st.UpdatedAt AS TaskUpdatedAt,
+
+    se.Id AS EventId,
+    se.Description AS EventDescription,
+    se.Location AS EventLocation,
+    se.StartTime,
+    se.EndTime,
+
+    u.UserName AS AssignedToUsername,
+    u.FullName AS AssignedToFullName,
+
+    sreq.Id AS RequestId,
+
+    sa.Description AS AnnouncementDescription,
+
+    a.Name AS AnimalName,
+    a.Species AS AnimalSpecies,
+    a.Breed AS AnimalBreed
+FROM SearchTasks st
+JOIN SearchEvents se ON st.EventId = se.Id
+JOIN SearchRequests sreq ON se.RequestId = sreq.Id
+JOIN SearchAnnouncements sa ON sreq.Id = sa.Id
+JOIN Animals a ON sa.AnimalId = a.Id
+LEFT JOIN Users u ON st.AssignedTo = u.Id;
+ 
+ -- select * from SearchEvents
 
 
 
 
 
+
+
+
+----------
+
+DROP TABLE SonarTasks
+DROP TABLE SonarProcess
+drop table SearchLeader
 
 
 -- <<<<<<<<<<  22.01.2025 настраиваю таблицу SonarTasks
